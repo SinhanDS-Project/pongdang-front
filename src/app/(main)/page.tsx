5b49501803686bb75d6f85d3e38d5f59aec74d7c
A Meaningful star <ghrlfehd123@naver.com>
2025-09-02 10:14:28 +0900

    edit: main page - 배너 슬라이드

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,27 @@
+import { Card, CardContent } from '@/components/ui/card'
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel'
+
+export default function MainHome() {
+  return (
+    <div className="lg:px-8) container mx-auto px-4 md:px-6">
+      {/* 배너 슬라이더 */}
+      <Carousel className="w-full">
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index}>
+              <div className="p-1">
+                <Card>
+                  <CardContent className="flex aspect-3/1 items-center justify-center p-6">
+                    <span className="text-4xl font-semibold">{index + 1}</span>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  )
+}
