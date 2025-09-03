5364a50470c42ba1c36769c6b2fa63fe1ded3675
A Meaningful star <ghrlfehd123@naver.com>
2025-09-03 12:32:11 +0900

    edit: [main-page]CtaSection 적용

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 
 import { AlertBar } from '@/components/main-page/AlertBar'
 import { Banner, BannerCarousel } from '@/components/main-page/BannerCarousel'
+import { CtaSection } from '@/components/main-page/CtaSection'
 import { Card, CardContent } from '@/components/ui/card'
 import { PongIcon } from '@/icons'
 import { serverFetchJSON } from '@/lib/net/server-fetch'
@@ -33,31 +34,18 @@ export default async function MainHome() {
   return (
     <div className="lg:px-8) container mx-auto px-4 md:px-6">
       <BannerCarousel banners={banners} />
-
-      {/* AI 추천 CTA 섹션 */}
-      <div className="bg-bubble mb-8 flex flex-col items-center gap-y-16 py-24">
-        <div className="text-primary-shinhan text-5xl font-extrabold">나에게 맞는 금융은 뭐가 있을까?</div>
-        <div className="flex flex-col items-center gap-y-8 text-3xl font-medium">
-          <div className="flex items-center justify-center gap-x-16">
-            <div className="animate-bounce [animation-delay:0s]">저축</div>
-            <div className="animate-bounce [animation-delay:0.2s]">투자</div>
-            <div className="animate-bounce [animation-delay:0.4s]">대출</div>
-            <div className="animate-bounce [animation-delay:0.6s]">카드</div>
-            <div className="animate-bounce [animation-delay:0.8s]">자산관리</div>
-          </div>
-          <div className="flex items-center justify-center gap-x-16">
-            <div className="animate-bounce [animation-delay:0.9s]">소비패턴</div>
-            <div className="animate-bounce [animation-delay:0.7s]">절약</div>
-            <div className="animate-bounce [animation-delay:0.5s]">보험</div>
-            <div className="animate-bounce [animation-delay:0.3s]">연금</div>
-            <div className="animate-bounce [animation-delay:0.1s]">주거/부동산</div>
-          </div>
-        </div>
-        <Link href={'#'} className="text-foreground hover:text-foreground/70 text-3xl font-bold underline">
-          나의 금융 소비에 맞는 AI 추천 받으러 가기
-        </Link>
-      </div>
       <AlertBar message="작은 퐁 하나가 내일의 큰 혜택이 됩니다" />
+      <CtaSection
+        title="나에게 맞는 금융은 뭐가 있을까?"
+        rows={
+          [
+            ['저축', '투자', '대출', '카드', '자산관리'],
+            ['소비패턴', '절약', '보험', '연금', '주거/부동산'],
+          ] as string[][]
+        }
+        ctaHref="#"
+        ctaLabel="나의 금융 소비에 맞는 AI 추천 받으러 가기"
+      />
 
       {/* 퐁당 스토어  */}
       <div className="mb-8 flex items-center text-3xl font-semibold">
