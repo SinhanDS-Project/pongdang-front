4e24949e282a66794cd45a6c747467e5dc29bb93
A Meaningful star <ghrlfehd123@naver.com>
2025-09-11 17:13:36 +0900

    ETC: UI에 필요한 icon.svg 파일
    - BackCoinIcon
    - DefaultCoinIcon
    - FailIcon
    - FrontCoinIcon
    - SuccessIcon
    - TitleCoinIcon

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -1,6 +1,23 @@
+import BackCoinIcon from './back_coin.svg'
+import DefaultCoinIcon from './default_coin.svg'
+import FailIcon from './fail.svg'
+import FrontCoinIcon from './front_coin.svg'
 import GameIcon from './game.svg'
 import HostIcon from './host.svg'
 import PlayerIcon from './player.svg'
 import PongIcon from './pong.svg'
+import SuccessIcon from './success.svg'
+import TitleCoinIcon from './title_coin.svg'
 
-export { GameIcon, HostIcon, PlayerIcon, PongIcon }
+export {
+  BackCoinIcon,
+  DefaultCoinIcon,
+  FailIcon,
+  FrontCoinIcon,
+  GameIcon,
+  HostIcon,
+  PlayerIcon,
+  PongIcon,
+  SuccessIcon,
+  TitleCoinIcon,
+}
