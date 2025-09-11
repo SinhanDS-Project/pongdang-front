470d1d92d07b7ce324cb03c5c4b7fc2b49ac373f
A Meaningful star <ghrlfehd123@naver.com>
2025-09-11 14:12:43 +0900

    ETC: UI에 필요한 icon.svg 파일
    - SingleBellIcon
    - SingleBombIcon
    - SingleCoinIcon

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -2,5 +2,9 @@ import GameIcon from './game.svg'
 import HostIcon from './host.svg'
 import PlayerIcon from './player.svg'
 import PongIcon from './pong.svg'
+import SingleBellIcon from './single_bell.svg'
+import SingleBombIcon from './single_bomb.svg'
+import SingleCoinIcon from './single_coin.svg'
 
-export { GameIcon, HostIcon, PlayerIcon, PongIcon }
+export { GameIcon, HostIcon, PlayerIcon, PongIcon, SingleBellIcon, SingleBombIcon, SingleCoinIcon }
+
