54faf9ffdc358a878919f714dc03f86d7e2d3037
gayun32 <prettygu2355@naver.com>
2025-09-15 09:35:09 +0900

    feat: 게시판 type 설정

---
+++ src/components/board-page/types.ts
@@ -0,0 +1,20 @@
+export type Category = 'NOTICE' | 'FREE' | 'EVENT'
+export type Board = {
+  id: number
+  title: string
+  content: string
+  created_at: Date
+  nickname: string
+  category: string
+  like_count: number
+  view_count: number
+  user_id: number
+}
+
+export type Reply = {
+  id: number
+  content: string
+  created_at: string
+  writer: string
+  user_id: number
+}
