0acd6efcee30abd2df445f3292b29e28cf270efd
gayun32 <prettygu2355@naver.com>
2025-09-15 09:34:01 +0900

    edit: 게시판 아이콘 추가

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -2,5 +2,6 @@ import GameIcon from './game.svg'
 import HostIcon from './host.svg'
 import PlayerIcon from './player.svg'
 import PongIcon from './pong.svg'
+import BoardIcon from './board.svg'
 
-export { GameIcon, HostIcon, PlayerIcon, PongIcon }
+export { GameIcon, HostIcon, PlayerIcon, PongIcon, BoardIcon }
