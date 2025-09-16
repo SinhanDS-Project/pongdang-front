5b02539deff03c7975016b2a2d539790bc48e8cb
A Meaningful star <ghrlfehd123@naver.com>
2025-09-16 12:37:45 +0900

    edit: 불필요한 코드 삭제

--- src/components/main-page/AlertBar.tsx
+++ src/components/main-page/AlertBar.tsx
@@ -1,16 +1,14 @@
 'use client'
 
-import { useEffect, useRef, useState } from 'react'
 import { Client, IMessage } from '@stomp/stompjs'
+import { useEffect, useRef, useState } from 'react'
 import SockJS from 'sockjs-client'
 
 export function AlertBar() {
-  console.log('안녕?')
   const [message, setMessage] = useState('작은 퐁 하나가 내일의 큰 혜택이 됩니다')
   const clientRef = useRef<Client | null>(null)
 
   useEffect(() => {
-    
     if (typeof window === 'undefined') return
 
     const wsUrl = process.env.NEXT_PUBLIC_WEBSOCKET_URL
@@ -21,12 +19,10 @@ export function AlertBar() {
       reconnectDelay: 2000,
       debug: () => {},
       onConnect: () => {
-        console.log('연결')
         client.subscribe('/topic/notice', (msg: IMessage) => {
           try {
             const payload = JSON.parse(msg.body)
             if (payload?.type === 'donation' && payload?.data) {
-              console.log('[STOMP] parsed payload:', payload)
               setMessage(String(payload.data))
             }
           } catch (e) {
