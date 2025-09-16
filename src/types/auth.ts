7de484d8ee0d883f5be59422cb7c355b7afc4f0c
A Meaningful star <ghrlfehd123@naver.com>
2025-09-16 17:48:08 +0900

    edit: 서버에서 받아오는 데이터 명 수정

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -5,7 +5,7 @@ export type User = {
   email: string
   phone_number: string
   birth_date: string // YYYY-MM-DD
-  profile_image: string | null
+  profile_img: string | null
   tutorial_check: boolean // 튜토리얼 완료 여부
   asso_check: boolean // 약관 동의 여부
   linked_with_betting: boolean // 베팅포인트 연동 여부
