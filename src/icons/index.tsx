af28253640133117994f92a21104afbf3e2794a9
A Meaningful star <ghrlfehd123@naver.com>
2025-09-22 17:46:22 +0900

    edit: UI에 필요한 Icon 파일 추가 및 수정
    - TrophyIcon
    - CoinsIcon

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -1,4 +1,5 @@
 import BackCoinIcon from './back_coin.svg'
+import CoinsIcon from './coins.svg'
 import DefaultCoinIcon from './default_coin.svg'
 import FailIcon from './fail.svg'
 import FrontCoinIcon from './front_coin.svg'
@@ -14,10 +15,12 @@ import SingleBombIcon from './single_bomb.svg'
 import SingleCoinIcon from './single_coin.svg'
 import SuccessIcon from './success.svg'
 import TitleCoinIcon from './title_coin.svg'
+import TrophyIcon from './trophy.svg'
 import YellowTurtleIcon from './yellow_turtle.svg'
 
 export {
   BackCoinIcon,
+  CoinsIcon,
   DefaultCoinIcon,
   FailIcon,
   FrontCoinIcon,
@@ -33,5 +36,6 @@ export {
   SingleCoinIcon,
   SuccessIcon,
   TitleCoinIcon,
+  TrophyIcon,
   YellowTurtleIcon,
 }
