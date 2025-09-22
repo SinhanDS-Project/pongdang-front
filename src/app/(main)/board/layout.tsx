95966fc6d66e76b718fe0e5f519f475f43632148
A Meaningful star <ghrlfehd123@naver.com>
2025-09-22 21:31:08 +0900

    edit: board page - layout 수정

--- src/app/(main)/board/layout.tsx
+++ src/app/(main)/board/layout.tsx
@@ -1,40 +1,32 @@
-import type { ReactNode } from 'react'
-
-import { BoardIcon } from '@/icons'
-
 import BoardTabs from '@/components/board-page/BoardTabs'
+import { BoardIcon } from '@/icons'
+import type { ReactNode } from 'react'
 
 export const metadata = { title: '게시판' }
 
 export default function BoardLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="mx-auto max-w-5xl px-4 pt-8 pb-3">
-      <header className="mb-2">
-        <div className="gap- flex items-center">
-          <div className="relative mt-4 aspect-[35/31] w-40 shrink-0 sm:w-56 md:w-[242px] lg:w-80">
-            <BoardIcon />
-          </div>
+    <div className="container mx-auto px-4 md:px-6 lg:px-8">
+      {/* 상단 헤더 */}
+      <div className="mb-8 flex flex-col items-center sm:mb-12 sm:flex-row sm:items-end sm:gap-x-8">
+        {/* 아이콘 영역 */}
+        <div className="bg-bubble rounded-full p-8 sm:p-12 md:p-16 lg:p-20">
+          <BoardIcon />
+        </div>
 
-          {/* 텍스트 */}
-          <div className="text-left">
-            <h1
-              className="text-[60px] leading-none font-bold tracking-tight text-[#0045FF]"
-              style={{ fontFamily: 'Pretendard' }}
-            >
-              게시판
-            </h1>
-
-            <p
-              className="mt-5 hidden text-[32px] leading-none font-bold text-[#6AADEF] md:block"
-              style={{ fontFamily: 'Pretendard' }}
-            >
-              우리의 생각과 소식이 모이는 곳
-            </p>
+        {/* 타이틀 영역 */}
+        <div className="flex grow flex-col items-center sm:mb-8 sm:items-start sm:justify-end sm:gap-y-4">
+          <div className="text-primary-shinhan text-3xl font-extrabold sm:text-4xl md:text-5xl lg:text-6xl">게시판</div>
+          <div className="text-secondary-sky text-lg font-semibold sm:text-2xl md:text-3xl lg:text-4xl">
+            우리의 생각과 소식이 모이는 곳
           </div>
         </div>
-      </header>
+      </div>
 
+      {/* 탭 */}
       <BoardTabs />
+
+      {/* 컨텐츠 */}
       {children}
     </div>
   )
