910afc8d14210c447d9a1e3b8721cad1ab5c56e6
gayun32 <prettygu2355@naver.com>
2025-09-24 16:59:08 +0900

    edit: 스토어 타입 import 경로 수정

--- src/components/store-page/ProductModal.tsx
+++ src/components/store-page/ProductModal.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Image from 'next/image'
-import { Product } from './types'
+import { Product } from '../../types/store'
 
 import { Badge } from '@/components/ui/badge'
 import { Button } from '@/components/ui/button'
