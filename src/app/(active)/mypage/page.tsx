6cec9718f8dd1a454c1a3b779e167d11d82279f2
YongHee <yonghee1234@gmail.com>
2025-09-25 11:30:12 +0900

    Revert "fix: 마이페이지 내역 조회 날짜 formatting 수정"
    This reverts commit c8001b4c23e7351fb6cd9cc770b3087b799dea12.

--- src/app/(active)/mypage/page.tsx
+++ src/app/(active)/mypage/page.tsx
@@ -290,11 +290,7 @@ export default function MyPageContent() {
 
 function formatDate(date: string | Date) {
   const parsedDate = typeof date === 'string' ? new Date(date) : date
-  return parsedDate.toLocaleDateString('ko-KR', {
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit',
-  }) // "2025. 09. 25."
+  return parsedDate.toISOString().split('T')[0] // "2025-09-03"
 }
 
 function PongHistory({ history, page, size }: { history: PongHistoryType[]; page: number; size: number }) {
